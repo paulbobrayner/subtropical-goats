aced1904fce1710b9eea4290c32cc2c661bc5cde
Paul <paulrobertrayner@hotmail.co.uk>
2021-09-15 07:54:51 +0100

    rating state

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+state = {
+  rating: 0,
+};
+
 function submitReview() {
   const review = document.getElementById('review-input').value;
-  console.log('REV', review);
   // POST rating / product id and review when submit
   // close modal also
+  console.log('rating', state.rating);
 }
 
 function toggleColour(id) {
-  console.log('ID', id);
   const color = document.getElementById(id).style.color;
   if (color == 'lightgrey') {
     document.getElementById(id).style.color = '#FEE12B';
+    state.rating++;
   } else {
     document.getElementById(id).style.color = 'lightgrey';
+    state.rating--;
   }
 }
 

de9e9681a74baa92c35d9c87ba13789644fe61de
Paul <paulrobertrayner@hotmail.co.uk>
2021-09-15 08:23:51 +0100

    loop over and display hardcoded reviews

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ state = {
   rating: 0,
 };
 
+const reviews = [
+  { id: 1, review: 'pretty average', rating: '3' },
+  { id: 2, review: 'cool', rating: '4' },
+  { id: 3, review: 'not so good', rating: '2' },
+];
+
+const htmlReview = reviews
+  .map((review) => {
+    return `<div style="display:flex"><i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin: 5px 10px 0'></i><div style="margin-top:6px"><strong>${review.rating}</strong>, ${review.review}</div></div>`;
+  })
+  .join('');
+
+document.getElementById('reviews').innerHTML = htmlReview;
+
 function submitReview() {
   const review = document.getElementById('review-input').value;
   // POST rating / product id and review when submit
---
+++ stars.js
@@ -0,0 +1,6 @@
+document.write(`          
+<i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star1')" id='star1'></i>
+<i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star2')" id='star2'></i>
+<i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star3')" id='star3'></i>
+<i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star4')" id='star4'></i>
+<i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star5')" id='star5'></i>`);

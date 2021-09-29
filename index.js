b66b0bdddf5349ebef7b2dd644c1e28ae935cad4
Paul <paulrobertrayner@hotmail.co.uk>
2021-09-29 08:19:40 +0100

    post review request

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ state = {
   rating: 0,
 };
 
+// would likely come from the looped over products on a previous page
+const productId = 1;
+const baseUrl = 'http://localhost:9080/api';
+
 const reviews = [
   { id: 1, review: 'pretty average', rating: 3 },
   { id: 2, review: 'cool', rating: 4 },
   { id: 3, review: 'not so good', rating: 2 },
 ];
+
+async function fetchProduct(url) {
+  let response = await fetch(url);
+  const data = await response.json();
+  document.getElementById('title').innerHTML = data?.name;
+
+  // do reviews stuff here - create func tho
+  // possibly total rating also
+}
+
+const product = fetchProduct(`${baseUrl}/product/${productId}`);
 
 function getTotalRating() {
   const total = reviews.reduce((a, b) => a + b.rating, 0);
@@ -39,14 +54,25 @@ const htmlReview = reviews
 
 const overallRating = getStars(getTotalRating());
 
+// do same with actual aveage rating - innerhtml
 document.getElementById('totalRating').innerHTML = overallRating;
 document.getElementById('reviews').innerHTML = htmlReview;
 
 function submitReview() {
   const review = document.getElementById('review-input').value;
-  // POST rating / product id and review when submit
-  // close modal also
-  console.log('submit review', state.rating, review);
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', `${baseUrl}/review`, true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(
+    JSON.stringify({
+      product_id: productId,
+      rating: state.rating,
+      review,
+    })
+  );
+  // close modal - re-fetch reviews
+  document.getElementById('closeButton').click();
+  // fetchReviews()
 }
 
 function toggleColour(id) {

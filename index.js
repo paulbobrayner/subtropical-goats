e9ab7c2fca5f6be2e6485d886c98301f19a68b36
Paul <paulrobertrayner@hotmail.co.uk>
2021-09-30 08:00:54 +0100

    get reviews request

--- index.js
+++ index.js
@@ -2,36 +2,27 @@ state = {
   rating: 0,
 };
 
-// would likely come from the looped over products on a previous page
+// id would likely come from the looped over products on a previous page
 const productId = 1;
 const baseUrl = 'http://localhost:9080/api';
-
-const reviews = [
-  { id: 1, review: 'pretty average', rating: 3 },
-  { id: 2, review: 'cool', rating: 4 },
-  { id: 3, review: 'not so good', rating: 2 },
-];
 
 async function fetchProduct(url) {
   let response = await fetch(url);
   const data = await response.json();
   document.getElementById('title').innerHTML = data?.name;
-
-  // do reviews stuff here - create func tho
-  // possibly total rating also
 }
 
-const product = fetchProduct(`${baseUrl}/product/${productId}`);
+fetchProduct(`${baseUrl}/product/${productId}`);
 
-function getTotalRating() {
+function getTotalRating(reviews) {
   const total = reviews.reduce((a, b) => a + b.rating, 0);
   const average = total / reviews.length;
 
-  return Math.round(average);
+  return average.toFixed(1);
 }
 
 function getStars(rating) {
-  // if index less than rating colour it yellow
+  // if index less than rating make it yellow
   const stars = ['1', '2', '3', '4', '5'];
   return stars
     .map((star, index) => {
@@ -41,27 +32,36 @@ function getStars(rating) {
     .join('');
 }
 
-// loop over reviews and add to html
-const htmlReview = reviews
-  .map((review) => {
-    return `<div style="display:flex; margin-top:15px;">${getStars(
-      review.rating
-    )}<div style="margin:3px 10px"><strong>${review.rating}</strong>, ${
-      review.review
-    }</div></div>`;
-  })
-  .join('');
+function getHtmlReviews(reviews) {
+  return reviews
+    .map((review) => {
+      return `<div style="display:flex; margin-top:15px;">${getStars(
+        review.rating
+      )}<div style="margin:3px 10px"><strong>${review.rating}</strong>, ${
+        review.review
+      }</div></div>`;
+    })
+    .join('');
+}
 
-const overallRating = getStars(getTotalRating());
+// get reviews and add to html
+async function fetchReviews(url) {
+  let response = await fetch(url);
+  const data = await response.json();
+  const averageRating = getTotalRating(data);
 
-// do same with actual aveage rating - innerhtml
-document.getElementById('totalRating').innerHTML = overallRating;
-document.getElementById('reviews').innerHTML = htmlReview;
+  document.getElementById('averageRating').innerHTML = averageRating;
+  document.getElementById('averageRatingStars').innerHTML =
+    getStars(averageRating);
+  document.getElementById('reviews').innerHTML = getHtmlReviews(data);
+}
+
+fetchReviews(`${baseUrl}/reviews/${productId}`);
 
 function submitReview() {
   const review = document.getElementById('review-input').value;
   let xhr = new XMLHttpRequest();
-  xhr.open('POST', `${baseUrl}/review`, true);
+  xhr.open('POST', `${baseUrl}/reviews`, true);
   xhr.setRequestHeader('Content-Type', 'application/json');
   xhr.send(
     JSON.stringify({
@@ -70,9 +70,8 @@ function submitReview() {
       review,
     })
   );
-  // close modal - re-fetch reviews
   document.getElementById('closeButton').click();
-  // fetchReviews()
+  fetchReviews(`${baseUrl}/reviews/${productId}`);
 }
 
 function toggleColour(id) {
--- stars.js
+++ stars.js
@@ -1,4 +1,4 @@
-document.write(`          
+document.write(`
 <i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star1')" id='star1'></i>
 <i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star2')" id='star2'></i>
 <i class="fas fa-star" style='font-size: 28px; color: lightgrey; margin-top: 5px  0' onClick="toggleColour('star3')" id='star3'></i>

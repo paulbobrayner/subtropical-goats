24dc349107968dfda11665390e97aa054b71379c
Paul <paulrobertrayner@hotmail.co.uk>
2021-10-17 09:59:13 +0100

    gitignore addition:

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import ProductContainer from './App/product/ProductContainer';
+import ProductContainer from './App/Product/ProductContainer';
 
 function App() {
   return <ProductContainer />;
